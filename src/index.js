62fa9f4b6691a63b21e377fbffbde4b0ae323e20
은배 <geb9211@gmail.com>
2023-04-01 23:38:55 +0900

    chore: 기초 세팅

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+import './reset.css';
+import './style.scss';
+
+function component() {
+  // const element = document.createElement('div');
+  // // 이제 Lodash를 스크립트로 가져왔습니다.
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // return element;
+}
+
+document.body.appendChild(component());
